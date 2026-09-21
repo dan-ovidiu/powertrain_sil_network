9b2de1050eb0a43021a41a8c39d388063e6dcade
Dan Ovidiu <ovi_enduro03@yahoo.com>
2026-09-21 19:19:26 +0300

    Remove CMake build artifacts from repository

--- CMakeFiles/network_sil.dir/Transmission_vECU/transmission.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/network_sil.dir/Transmission_vECU/transmission.c.obj: \
- C:\Users\Dan\ Ovidiu\Documents\antigravity\keen-newton\powertrain_sil_network\Transmission_vECU\transmission.c \
- C:\Users\Dan\ Ovidiu\Documents\antigravity\keen-newton\powertrain_sil_network\Transmission_vECU\transmission.h \
- C:/Users/Dan\ Ovidiu/Documents/antigravity/keen-newton/powertrain_sil_network/Virtual_CAN/vcan.h
--- CMakeFiles/network_sil.dir/Virtual_CAN/vcan.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/network_sil.dir/Virtual_CAN/vcan.c.obj: \
- C:\Users\Dan\ Ovidiu\Documents\antigravity\keen-newton\powertrain_sil_network\Virtual_CAN\vcan.c \
- C:\Users\Dan\ Ovidiu\Documents\antigravity\keen-newton\powertrain_sil_network\Virtual_CAN\vcan.h
